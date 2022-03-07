4f5eae6c4fb99f7b41d85033975aeccdc2ef53e2
Aleksa-esme <esme1990@mail.ru>
2022-03-07 19:32:42 +0300

    lesson 2

--- src/Message.js
+++ src/Message.js
@@ -1,10 +1,12 @@
-import './Message.css';
+
 
 export function Message(props) {
   return (
     <div className="Message">
       <header className="Message-header">
-        <h3 className="Message-text">{props.text}</h3>
+        <p className="Message-text">Сообщение №{props.index}</p>
+        <p className="Message-text">Автор: {props.author}</p>
+        <p className="Message-text">Тескт: {props.text}</p>
       </header>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,19 +1,45 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import ReactDOM from 'react-dom';
 import {Message} from './Message';
 
 import './index.css';
 
+function App() {
+  const [messageList, setMessageList] = useState([]);
+  const [inputText, setInputText] = useState('');
 
-const myText = <p>My name is Aleksa!</p>;
 
+  
 
+  const inputValue = (e) => {
+    setInputText(e.target.value);
+  }
 
+  const addText = () => {
+    addMessage('user0', inputText);
+    setInputText('');
+  }
+
+  const addMessage = (user, text) => {
+    setMessageList([...messageList, {author: user, text: text}]);
+  }
+
+  return (
+    <div>
+      <form>
+        <input onChange={inputValue} type='text' text={inputText} placeholder='Ваше сообщение...'></input>
+        <button onClick={addText}>Отправить</button>  
+      </form>
+      
+        {messageList.map((message, index) => {
+          return (<Message index={index + 1} author={message.author} text={message.text} />)})}
+           
+    </div>
+  )
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Message text={myText}/>
-  </React.StrictMode>,
+  <App />,
   document.getElementById('root')
 );
 
