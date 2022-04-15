3803dc1aead412b9950105b3eb72f7effd086e80
Aleksa-esme <esme1990@mail.ru>
2022-04-15 14:03:47 +0300

    make messages a part of conversations in firebase DB

--- src/api/messages.js
+++ src/api/messages.js
@@ -2,9 +2,9 @@ import { child, ref, get, push } from "firebase/database";
 import { database } from "./firebase";
 
 export const createMessageApi = (message, roomId) => {
-  return push(child(ref(database), `messages/${roomId}`), message);
+  return push(child(ref(database), `conversations/${roomId}/messages`), message);
 };
 
 export const getMessagesApi = () => {
-  return get(child(ref(database), `messages`));
+  return get(child(ref(database), `conversations/messages`));
 };
