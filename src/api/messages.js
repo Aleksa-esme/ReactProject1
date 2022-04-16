2a4722ba4f405ec55dd3c2a81ce11b86060c13ff
Aleksa-esme <esme1990@mail.ru>
2022-04-16 14:23:33 +0300

    *

--- src/api/messages.js
+++ src/api/messages.js
@@ -2,9 +2,12 @@ import { child, ref, get, push } from "firebase/database";
 import { database } from "./firebase";
 
 export const createMessageApi = (message, roomId) => {
-  return push(child(ref(database), `conversations/${roomId}/messages`), message);
+  return push(child(ref(database), `messages/${roomId}`), message);
 };
 
 export const getMessagesApi = () => {
-  return get(child(ref(database), `conversations/messages`));
+  return get(child(ref(database), `messages`));
 };
+
+
+//переделать пути позже
